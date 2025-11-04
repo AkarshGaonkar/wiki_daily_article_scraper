36e0b57a566ef0c013493346177ba6ff4835b6ff
AkarshGaonkar <152944826+AkarshGaonkar@users.noreply.github.com>
2025-11-04 10:34:16 -0500

    Add files via upload

---
+++ CLASS19_Wiki_scraper_regular.R
@@ -0,0 +1,39 @@
+# featured wiki article selection
+
+# get_wikipedia_daily.R
+
+library(rvest)
+library(dplyr)
+library(readr)
+
+url <- "https://en.wikipedia.org/wiki/Main_Page"
+
+page <- read_html(url)
+
+# Extract the "Today's featured article" section
+featured <- page |>
+  html_node("#mp-tfa p") |>
+  html_text(trim = TRUE)
+
+# Extract the article link
+featured_link <- page |>
+  html_node("#mp-tfa b a") |>     # first <a> in the section
+  html_attr("href")
+
+featured_url <- paste0("https://en.wikipedia.org", featured_link)
+
+entry <- tibble(
+  datetime = format(Sys.time(), tz = "America/New_York", usetz = TRUE),
+  featured_article = featured,
+  link = featured_url
+)
+
+dir.create("data", showWarnings = FALSE)
+file <- "data/wikipedia_daily.csv"
+
+# Append if exists, otherwise create
+if (file.exists(file)) {
+  write_csv(entry, file, append = TRUE, col_names = FALSE)
+} else {
+  write_csv(entry, file)
+}
